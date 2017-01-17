3992814ae7836eb022d5179d56bf1fdd1418ba1a
Zaar Hai <haizaar@haizaar.com>
2017-01-17 16:19:53 +0200

    using cache to speedup generation

--- names/__init__.py
+++ names/__init__.py
@@ -1,6 +1,7 @@
 from __future__ import unicode_literals
 from os.path import abspath, join, dirname
 import random
+import bisect
 
 
 __title__ = 'names'
@@ -18,9 +19,25 @@ FILES = {
     'last': full_path('dist.all.last'),
 }
 
+CACHE = {}
 
 def get_name(filename):
     selected = random.random() * 90
+    if filename in CACHE:
+        return get_name_from_cache(filename, selected)
+    else:
+        return get_name_from_file(filename, selected)
+
+
+def get_name_from_cache(filename, selected):
+    cache = CACHE[filename]
+    i = bisect.bisect_right(cache, selected)
+    if i != len(cache):
+        return cache[i].name
+    return ""
+
+
+def get_name_from_file(filename, selected):
     with open(filename) as name_file:
         for line in name_file:
             name, _, cummulative, _ = line.split()
@@ -43,3 +60,24 @@ def get_last_name():
 
 def get_full_name(gender=None):
     return "{0} {1}".format(get_first_name(gender), get_last_name())
+
+
+def init_cache():
+    for full_path in FILES.values():
+        CACHE[full_path] = _build_cache(full_path)
+
+
+def _build_cache(filename):
+    cache = list()
+    for line in  open(filename):
+        name, _, cummulative, _ = line.split()
+        cummulative = float(cummulative)
+        item = Name(cummulative)
+        item.name = name
+        cache.append(item)
+    return cache
+
+
+class Name(float):
+    "Extending float (rank) to hold its name"
+    pass
